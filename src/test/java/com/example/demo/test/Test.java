8eee7617379beced17aebd8159f0d90b7e5f69d2
yxie <yxie@skystartrade.com>
2019-08-13 14:23:46 +0800

    mybatis plus

--- src/test/java/com/example/demo/test/Test.java
+++ src/test/java/com/example/demo/test/Test.java
@@ -4,7 +4,6 @@ import com.example.demo.service.impl.SysUserServiceImpl;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +18,7 @@ public class Test {
     @org.junit.Test
     public void test() {
 
-        System.out.println(sysUserService.updateUser());
+//        System.out.println(sysUserService.updateUser());
         System.out.println(sysUserService.selectList());
     }
 
